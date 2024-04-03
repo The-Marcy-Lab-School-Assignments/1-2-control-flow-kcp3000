cdd8f74bbe0fc64456b0c70717928407748a330f
Kevin <mrkev63@gmail.com>
2024-04-03 12:39:20 -0400

    finished q1 & 2

--- src/bonus-switch.js
+++ src/bonus-switch.js
@@ -1,4 +1,23 @@
-const measureRainSwitch = () => {
+const measureRainSwitch = (inc) => {
+  switch (true) {
+    case inc === 0:
+      console.log('drought');
+      break;
+    case inc < 2:
+      console.log(`dry`);
+      break;
+    case inc < 4:
+      console.log(`average`);
+      break;
+    case inc < 6:
+      console.log(`rainy`);
+      break;
+    case inc >= 6:
+      console.log(`flood`);
+      break;
+    default:
+      console.log(`what?`);
+  };
 };
 
 const rounderSwitch = () => {
--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,8 +1,49 @@
-const measureRain = () => {
+const measureRain = (inc) => {
+  switch (true) {
+    case inc === 0:
+      console.log('drought');
+      break;
+    case inc < 2:
+      console.log(`dry`);
+      break;
+    case inc < 4:
+      console.log(`average`);
+      break;
+    case inc < 6:
+      console.log(`rainy`);
+      break;
+    case inc >= 6:
+      console.log(`flood`);
+      break;
+    default:
+      console.log(`what?`);
+  };
 };
 
-const happyBirthdayPet = () => {
-};
+const happyBirthdayPet = (breed, age) => {
+  switch (true) {
+    case (breed === 'snake'):
+      console.log('Hiss hiss!');
+      break;
+    case (breed === 'cat' && age < 5):
+      console.log('Mew mew!');
+      break;
+    case (breed === 'dog' && age < 5):
+      console.log('Arf arf!');
+      break;
+    case (breed === 'cat' && age <= 10 && age >= 5):
+      console.log('Meow meow!');
+      break;
+    case (breed === 'dog' && age <= 9 && age >= 5):
+      console.log('Woof woof!');
+      break;
+    case (breed === 'dog' && age <= 11):
+      console.log('Boof!')
+      break;
+    default:
+      console.log('Happy birthday!');
+  };
+}
 
 const funTypes = () => {
 };
